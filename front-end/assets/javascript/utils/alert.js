1454399f225df790a6675415f66b9a89ae3e159f
pedrosilvabr <pedro.com@gmail.com>
2021-09-05 19:38:24 -0300

    Conceito visual versão 1.0

--- front-end/assets/javascript/utils/alert.js
+++ front-end/assets/javascript/utils/alert.js
@@ -1,15 +1,20 @@
 function alert(atencion, msg, type) {
-    
-        console.log(msg)
 
-        if(atencion === 'dev'){ type = 'secondary' }
-        let alert = document.getElementById('alert')
-        alert.innerHTML = `
+    console.log(msg)
+
+    if (atencion === 'dev') { type = 'secondary' }
+    let alert = document.getElementsByClassName('container container-alert')
+
+    for (let i in alert) {
+
+        alert[i].innerHTML = `
         <div class="alert alert-${type} alert-dismissible fade show" role="alert">
             <h4><strong>${atencion}</strong></h4>
             <hr>
             <p>${msg}</p>
             <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
         </div>`
+
+    }
 
 }
