12e7a8fe5da6a456d836c6c48d35ae537562a68d
pedrosilvabr <pedro.com@gmail.com>
2021-09-05 22:27:45 -0300

    Terminal SQL

--- back-end/database/mssql/.console.sql
+++ back-end/database/mssql/.console.sql
@@ -1,5 +1,1 @@
-SELECT aes as entity, crdate FROM SYSOBJECTS WHERE NAME = 'aes' AND XTYPE = 'U';
-SELECT conteudo as entity, crdate FROM SYSOBJECTS WHERE NAME = 'conteudo' AND XTYPE = 'U';
-SELECT curso as entity, crdate FROM SYSOBJECTS WHERE NAME = 'curso' AND XTYPE = 'U';
-SELECT escola as entity, crdate FROM SYSOBJECTS WHERE NAME = 'escola' AND XTYPE = 'U';
-SELECT pessoa as entity, crdate FROM SYSOBJECTS WHERE NAME = 'pessoa' AND XTYPE = 'U'
+sp_help aes
--- socket.io/call.js
+++ socket.io/call.js
@@ -2,18 +2,100 @@ const package = require('./package')
 
 function call(io) {
     io.on('connection', socket => {
+
         socket.on('call', received => {
+
             if (received) {
+
                 // desenvolver tratamento de segurança
                 if (received.method && received.payload && !received.error) {
-                    require('./api')(socket, received)
+
+                    switch (received.method) {
+                        case 'api':
+                            require('./api')(socket, received)
+                            break;
+
+                        case 'sql':
+
+                            // tem que melhorar MUITO isso
+                            let queryToParse = received.payload
+                                , error = queryToParse.toUpperCase().substring(0,6) != 'SELECT'
+
+                            if (error) {
+
+                                socket.emit('call', package('sql', `query recusada ${queryToParse}`, true))
+
+                            } else {
+
+                                let forbidden = [
+
+                                    'drop',
+                                    'create',
+                                    'exec',
+                                    'update',
+                                    'delete',
+                                    'insert',
+                                    'sp_'
+
+                                ]
+
+                                error = forbidden.indexOf(queryToParse) > 0
+
+                                if (!error) {
+
+                                    const conn = require('../back-end/database/mssql/conn')
+                                    const sql = require('mssql')
+
+                                    let query = received.payload
+                                        , db = conn['aes'] // em desenvolvimento, virá do tenant
+
+                                    sql.connect(db).then(() => { return sql.query(query) })
+                                        .then(result => {
+
+                                            sql.close();
+                                            socket.emit('call', package('sql', result, false))
+
+                                        })
+                                        .catch(err => {
+
+                                            sql.close();
+                                            socket.emit('call', package('sql', err, true))
+
+                                        })
+                                    sql.on('error', err => {
+
+                                        socket.emit('call', package('sql', err, true))
+
+                                    })
+
+                                } else {
+
+                                    socket.emit('call', package('sql', `query recusada`, true))
+
+                                }
+
+                            }
+                            break;
+
+                        default:
+                            socket.emit('call', package(received.method, `método desconhecido: ${received.method}`, true))
+                            break;
+                    }
+
                 } else {
+
                     socket.emit('call', package(undefined, 'pacote corrompido', true))
+
                 }
+
             } else {
+
                 socket.emit('call', package(undefined, 'pedido rejeitado pelo servidor', true))
+
             }
+
         })
+
     })
 }
 module.exports = call
