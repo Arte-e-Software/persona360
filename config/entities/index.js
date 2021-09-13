d11f91a1d5aa80dfdcf49667c8da2afb054b6923
pedrosilvabr <pedro.com@gmail.com>
2021-09-13 18:38:15 -0300

    Mais ajustes UIX

--- config/entities/index.js
+++ config/entities/index.js
@@ -4,9 +4,6 @@ const fs = require('fs')
 
 module.exports = [
 
-    { "name": "pessoa", "json": "pessoa.json" },
-    { "name": "curso", "json": "curso.json" },
-    { "name": "escola", "json": "escola.json" },
-    { "name": "conteudo", "json": "conteudo.json" },
+   
 
 ]
