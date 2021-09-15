a9c63bfae9b4189d33130b8b76056014a9f6a1f5
pedrosilvabr <pedro.com@gmail.com>
2021-09-15 18:34:09 -0300

    /adm para /framework

---
+++ back-end/routes/framework.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const router = express.Router()
+const env = require('dotenv')
+
+module.exports = router.get('/framework', (req, res) => {
+
+    let entities = {}
+        , payload = {}
+        , env = process.env.NODE_ENV
+
+    try {
+
+        entities = require('../../config/entities')
+        payload = { "entities": entities, "err": false }
+        res.render('pages/framework', { "env": env, "title": 'aes/framework', "payload": payload, "err": false }) // em desenvolvimento, 'aes/framework' virá do handlerTenant
+
+    } catch(err) {
+
+        payload = { "entities": entities, "err": err }
+        res.render('pages/framework', { "env": env, "title": 'aes/framework', "payload": payload, "err": true }) // em desenvolvimento, 'aes/framework' virá do handlerTenant
+
+    }
+
+})
