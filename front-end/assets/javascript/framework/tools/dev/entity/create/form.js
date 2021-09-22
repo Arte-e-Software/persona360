a7385a03eca645d9ec349fbe51bd9a0dd58abed1
pedrosilvabr <pedro.com@gmail.com>
2021-09-22 02:29:45 -0300

    Cheguei no finish.js imprimindo  na tela form e entity.

--- front-end/assets/javascript/framework/tools/dev/entity/create/form.js
+++ front-end/assets/javascript/framework/tools/dev/entity/create/form.js
@@ -1,73 +1,65 @@
 window.onload = () => {
 
-  // #issue: pensar em como lidar melhor com os filtros
-  // #issue: poderia vir de um config.json
-  let filter = ',ABCDEFGHIJKLMNOPQRSTUVXWYZabcdefghijklmnopqrstuvxwyz0123456789_ '
+    // #issue: poderia vir de um config.json
+    const form = {
 
-  // #issue: poderia vir de um config.json
-  let form = {
+        tab: {
+            create: document.getElementById('entity-create-tab')
+        },
 
-    tab: {
-      create: document.getElementById('entity-create-tab')
-    },
+        name: {
+            alert: document.getElementById('alert-entity-name'),
+            input: document.getElementById('input-text-entity-name'),
+            trigger: 'keypress',
+            filter: ',ABCDEFGHIJKLMNOPQRSTUVXWYZabcdefghijklmnopqrstuvxwyz0123456789_ '
+        },
 
-    name: {
-      alert: document.getElementById('alert-entity-name'),
-      input: document.getElementById('input-text-entity-name'),
-      trigger: 'keypress',
-      filter: filter,
-      draft: draft
-    },
+        namespace: {
+            alert: document.getElementById('alert-entity-namespace'),
+            input: document.getElementById('input-textarea-entity-namespace'),
+            trigger: 'keypress',
+            filter: ',ABCDEFGHIJKLMNOPQRSTUVXWYZabcdefghijklmnopqrstuvxwyz0123456789_ '
+        },
 
-    namespace: {
-      alert: document.getElementById('alert-entity-namespace'),
-      input: document.getElementById('input-textarea-entity-namespace'),
-      trigger: 'keypress',
-      filter: filter,
-      draft: draft
-    },
+        fields: {
+            alert: document.getElementById('alert-entity-fields'),
+            settings: document.getElementById('fields-settings')
+        },
 
-    fields: {
-      alert: document.getElementById('alert-entity-fields'),
-      name: document.getElementById('entity-fields'),
-      settings: document.getElementById('fields-settings')
-    },
+        table: {
+            name: document.getElementById('table-name')
+        },
 
-    table: {
-      name: document.getElementById('table-name')
-    },
-
-    button: {
-      restart: document.getElementById('btn-entity-create-restart'),
-      create: document.getElementById('btn-entity-create')
+        button: {
+            restart: document.getElementById('btn-entity-create-restart'),
+            create: document.getElementById('btn-entity-create')
+        }
     }
 
-  }
+    // #issue: poderia ter um orquestrador global
+    // #issue: poderia vir de um config.json
+    enter(form, 'name')
+    enter(form, 'namespace')
 
-  // #issue: poderia ter um orquestrador global
-  // #issue: poderia vir de um config.json
-  enter(form, 'name')
-  enter(form, 'namespace')
+    form.tab.create.addEventListener('click', event => {
 
-  form.tab.create.addEventListener('click', event => {
+        event.preventDefault()
+        form.name.input.focus()
 
-    event.preventDefault()
-    form.name.input.focus()
+    })
 
-  })
+    form.button.restart.addEventListener('click', event => {
 
-  form.button.restart.addEventListener('click', event => {
+        // #issue: pensar em como mapear forms nas entidades
+        event.preventDefault()
+        form.name.input.focus()
+        form.name.input.value = ''
+        form.namespace.input.value = ''
+        form.fields.settings.innerHTML = ''
+        form.name.input.className = 'form-control is-invalid'
+        form.namespace.input.className = 'form-control is-invalid'
+        form.button.create.setAttribute('disabled', true)
 
-    // #issue: pensar em como mapear forms nas entidades
-    event.preventDefault()
-    form.name.input.focus()
-    form.name.input.value = ''
-    form.namespace.input.value = ''
-    form.fields.config.innerHTML = ''
-    form.name.input.className = 'form-control is-invalid'
-    form.namespace.input.className = 'form-control is-invalid'
-    form.button.create.disabled = true
-
-  })
+    })
 
 }
--- front-end/assets/javascript/framework/tools/dev/entity/create/settings.js
+++ front-end/assets/javascript/framework/tools/dev/entity/create/settings.js
@@ -1,17 +1,15 @@
-function settings(namespace) {
+const settings = namespace => {
 
-  let name = ''
-    , tbody = []
+    let settings = []
 
-  for (let field in namespace) {
+    for (let field in namespace) {
 
-    name = namespace[field].toLowerCase().trim()
-    tbody.push(`<tr>
+        settings.push(`<tr>
                             <td>
-                                <input class="form-control is-valid" type="text" id="field-${name}-name-input" entityfieldformtype="text" value="${name}" readonly>
+                                <input class="form-control is-valid" type="text" id="field-${namespace[field].toLowerCase().trim()}-name-input" value="${namespace[field].toLowerCase().trim()}" readonly>
                             </td>
-                            <td class="text-center" entityfieldformtype="select">
-                                <select class="form-select select-privacy is-valid" aria-label="lgpd" id="field-${name}-privacy">
+                            <td class="text-center">
+                                <select class="form-select select-privacy is-valid" aria-label="lgpd" id="field-${namespace[field].toLowerCase().trim()}-privacy">
                                     <option value="pessoal">Pessoal</option>
                                     <option value="comercial">Comercial</option>
                                     <option value="sensivel" selected>Sensível</option>
@@ -19,29 +17,29 @@ function settings(namespace) {
                                     <option value="anomatizado">Anonimizado</option>
                                 </select>
                             </td>
-                            <td class="text-center" entityfieldformtype="select">
-                                <select class="form-select select-type is-valid" aria-label="lgpd" id="field-${name}-type">
+                            <td class="text-center">
+                                <select class="form-select select-type is-valid" aria-label="lgpd" id="field-${namespace[field].toLowerCase().trim()}-type">
                                     <option value="number">Número</option>
                                     <option value="string" selected>Texto</option>
                                     <option value="date">Data</option>
                                     <option value="boolean">Booleano</option>
                                 </select>
                             </td>
-                            <td class="text-center" entityfieldformtype="number">
-                            <input type="number" class="form-control is-valid input-length" id="field-${name}-length" value="50">
+                            <td class="text-center">
+                            <input type="number" class="form-control is-valid input-length" id="field-${namespace[field].toLowerCase().trim()}-length" value="50">
                             </td>
                             <td class="text-left mr-o pr-0">
                                 <div class="form-check form-switch pr-0 mr-0 align-middle">
-                                    <input class="form-check-input is-valid checkbox-nullable" type="checkbox" id="field-${name}-nullable" entityfieldformtype="checkbox">
+                                    <input class="form-check-input is-valid checkbox-nullable" type="checkbox" id="field-${namespace[field].toLowerCase().trim()}-nullable">
                                 </div>
                             </td>
-                            <td class="text-center" id="field-${name}-searcheble">
+                            <td class="text-center" id="field-${namespace[field].toLowerCase().trim()}-searcheble">
                                 <div class="form-check form-switch pr-0 mr-0 align-middle">
-                                    <input class="form-check-input is-valid checkbox-searchable" type="checkbox" id="field-${name}-searchable" entityfieldformtype="checkbox" checked>
+                                    <input class="form-check-input is-valid checkbox-searchable" type="checkbox" id="field-${namespace[field].toLowerCase().trim()}-searchable" checked>
                                 </div>
                             </td>
                         </tr>`)
-  }
+    }
 
-  return tbody.join('')
+    return settings.join('')
 }
