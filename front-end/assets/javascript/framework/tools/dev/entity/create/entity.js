046ed520968d57b62bcfd3ed57d32baf6606d8af
pedrosilvabr <pedro.com@gmail.com>
2021-09-24 21:23:34 -0300

    Teaser

--- front-end/assets/javascript/framework/tools/dev/entity/create/entity.js
+++ front-end/assets/javascript/framework/tools/dev/entity/create/entity.js
@@ -1,119 +1,119 @@
 const entity = form => {
 
-  let entity = {
-    "name": form.name.input.value,
-    "namespace": form.namespace.input.value.split(',').map(name => { return name.trim() }),
-    "fields": []
-  }
+    let entity = {
+        "name": form.name.input.value,
+        "namespace": form.namespace.input.value.split(',').map(name => { return name.trim() }),
+        "fields": []
+    }
 
-  if (form.name.input.value) {
+    if (form.name.input.value) {
 
-    form.name.input.className = 'form-control is-valid'
-    form.namespace.input.focus()
+        form.name.input.className = 'form-control is-valid'
+        form.namespace.input.focus()
 
-    if (form.namespace.input.value) {
+        if (form.namespace.input.value) {
 
-      form.namespace.input.className = 'form-control is-valid'
+            form.namespace.input.className = 'form-control is-valid'
 
-      let draft = {
+            let draft = {
 
-        "fields": {
-          "settings": form.fields.settings,
-          "privacy": document.getElementsByClassName('select-privacy'),
-          "type": document.getElementsByClassName('select-type'),
-          "length": document.getElementsByClassName('input-length'),
-          "nullable": document.getElementsByClassName('checkbox-nullable'),
-          "searchable": document.getElementsByClassName('checkbox-searchable')
+                "fields": {
+                    "settings": form.fields.settings,
+                    "privacy": document.getElementsByClassName('select-privacy'),
+                    "type": document.getElementsByClassName('select-type'),
+                    "length": document.getElementsByClassName('input-length'),
+                    "nullable": document.getElementsByClassName('checkbox-nullable'),
+                    "searchable": document.getElementsByClassName('checkbox-searchable')
+                }
+
+            }
+
+            draft.fields.settings.innerHTML = settings(form.namespace.input.value.split(','))
+            draft.fields.privacy[0].focus()
+
+            console.log(draft)
+
+            for (let field in entity.namespace) {
+
+                entity.fields.push({
+
+                    "name": entity.namespace[field],
+                    "settings": {
+                        "privacy": draft.fields.privacy[field].options[draft.fields.privacy[field].selectedIndex].value,
+                        "type": draft.fields.type[field].options[draft.fields.type[field].selectedIndex].value,
+                        "length": draft.fields.length[field].value,
+                        "nullable": draft.fields.nullable[field].value,
+                        "searchable": draft.fields.searchable[field].value
+                    }
+
+                })
+
+            }
+
+            entity.namespace.push('crdate')
+            entity.namespace.push('isactive')
+            entity.namespace.unshift(`id_${entity.name}`)
+
+            entity.fields.push({
+
+                "name": `crdate`,
+                "settings": {
+                    "privacy": 'anonimizado',
+                    "type": 'date',
+                    "length": '',
+                    "nullable": false,
+                    "searchable": true
+                }
+
+            })
+
+            entity.fields.push({
+
+                "name": `isactive`,
+                "settings": {
+                    "privacy": 'anonimizado',
+                    "type": 'boolean',
+                    "length": '',
+                    "nullable": false,
+                    "searchable": true
+                }
+
+            })
+
+            entity.fields.unshift({
+
+                "name": `id_${entity.name}`,
+                "settings": {
+                    "privacy": 'anonimizado',
+                    "type": 'number',
+                    "length": '',
+                    "nullable": false,
+                    "searchable": true
+                }
+
+            })
+
         }
+    }
 
-      }
+    if (entity) {
 
-      draft.fields.settings.innerHTML = settings(form.namespace.input.value.split(','))
-      //draft.fields.privacy[0].focus()
+        form.button.create.setAttribute('disable', false)
+            // #issue: falta adicionar listen nas mudanças dos settings
+        form.entitysql.innerHTML = `<tr><td>${pretty(entity)}</td></tr>`
 
-      console.log(draft)
+        serverfunction('../back-end/database/mssql/templates/create-table', entity, (res) => {
 
-      for (let field in entity.namespace) {
-
-        entity.fields.push({
-
-          "name": entity.namespace[field],
-          "settings": {
-            "privacy": draft.fields.privacy[field].options[draft.fields.privacy[field].selectedIndex].value,
-            "type": draft.fields.type[field].options[draft.fields.type[field].selectedIndex].value,
-            "length": draft.fields.length[field].value,
-            "nullable": draft.fields.nullable[field].value,
-            "searchable": draft.fields.searchable[field].value
-          }
+            return form.sql.innerHTML = `<tr><td>${res.split(';').join(';<br>')}</td></tr>`
 
         })
 
-      }
+    } else {
 
-      entity.namespace.push('crdate')
-      entity.namespace.push('isactive')
-      entity.namespace.unshift(`id_${entity.name}`)
-
-      entity.fields.push({
-
-        "name": `crdate`,
-        "settings": {
-          "privacy": 'anonimizado',
-          "type": 'date',
-          "length": '',
-          "nullable": false,
-          "searchable": true
-        }
-
-      })
-
-      entity.fields.push({
-
-        "name": `isactive`,
-        "settings": {
-          "privacy": 'anonimizado',
-          "type": 'boolean',
-          "length": '',
-          "nullable": false,
-          "searchable": true
-        }
-
-      })
-
-      entity.fields.unshift({
-
-        "name": `id_${entity.name}`,
-        "settings": {
-          "privacy": 'anonimizado',
-          "type": 'number',
-          "length": '',
-          "nullable": false,
-          "searchable": true
-        }
-
-      })
+        return
 
     }
-  }
-
-  if (entity) {
-
-    form.button.create.setAttribute('disable', false)
-    // #issue: falta adicionar listen nas mudanças dos settings
-    form.entitysql.innerHTML = `<tr><td>${pretty(entity)}</td></tr>`
-
-    serverfunction('../back-end/database/mssql/templates/create-table', entity, (res) => {
-
-      return form.sql.innerHTML = `<tr><td>${res.split(';').join(';<br>')}</td></tr>`
-
-    })
-
-  } else {
 
     return
-
-  }
-
-  return
 
 }
