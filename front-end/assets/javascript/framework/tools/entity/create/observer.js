2f990fb5e3329ed6677aab576cd7793b97b84ef0
pedrosilvabr <pedro.com@gmail.com>
2021-09-27 12:09:38 -0300

    ajuste logo na dock para ficar mais responsivo

--- front-end/assets/javascript/framework/tools/entity/create/observer.js
+++ front-end/assets/javascript/framework/tools/entity/create/observer.js
@@ -1,39 +1,38 @@
 const observer = entity => {
 
-    let handler = (entity, id, newValue) => {
+    let change = (entity, id, value) => {
 
         let field = id.split('-')[1],
             setting = id.split('-')[2]
 
         console.log(entity)
-        console.log(field, setting, newValue)
+        console.log(field, setting, value)
         // #issue: parei aqui
 
     }
     for (let i = 1; i < entity.fields.length - 2; i++) {
         document.getElementById(`field-${entity.fields[i].name}-privacy`).addEventListener('change', event => {
             event.code === 'Enter' || 'NumpadEnter' ? event.preventDefault() : false
-            handler(entity, event.target.id, event.target.options[event.target.selectedIndex].value)
+            change(entity, event.target.id, event.target.options[event.target.selectedIndex].value)
         })
-
         document.getElementById(`field-${entity.fields[i].name}-type`).addEventListener('change', event => {
             event.code === 'Enter' || 'NumpadEnter' ? event.preventDefault() : false
-            handler(entity, event.target.id, event.target.options[event.target.selectedIndex].value)
+            change(entity, event.target.id, event.target.options[event.target.selectedIndex].value)
         })
         document.getElementById(`field-${entity.fields[i].name}-length`).addEventListener('change', event => {
-            handler(entity, event.target.id, event.target.value)
+            change(entity, event.target.id, event.target.value)
         })
         document.getElementById(`field-${entity.fields[i].name}-nullable`).addEventListener('change', event => {
             event.code === 'Enter' || 'NumpadEnter' ? event.preventDefault() : false
-            handler(entity, event.target.id, event.target.checked)
+            change(entity, event.target.id, event.target.checked)
         })
         document.getElementById(`field-${entity.fields[i].name}-hash`).addEventListener('change', event => {
             event.code === 'Enter' || 'NumpadEnter' ? event.preventDefault() : false
-            handler(entity, event.target.id, event.target.checked)
+            change(entity, event.target.id, event.target.checked)
         })
         document.getElementById(`field-${entity.fields[i].name}-searchable`).addEventListener('change', event => {
             event.code === 'Enter' || 'NumpadEnter' ? event.preventDefault() : false
-            handler(entity, event.target.id, event.target.checked)
+            change(entity, event.target.id, event.target.checked)
         })
     }
 }
